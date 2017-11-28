8d557fd0c73729512e6ca1f56aa0a797068d0dca
jborbon <jborbon@live.com>
2017-11-28 12:31:01 -0500

    added stylesheets for app and document component and moved style code into its own component css file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+
 import { Component } from '@angular/core';
 
 @Component({
 	moduleId: module.id,
 	selector: 'app',
-	templateUrl: "app.component.html"
+	templateUrl: "app.component.html",
+	styleUrls:["app.component.css"]
 	
 })
 
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,35 +1,37 @@
-import {Component} from '@angular/core';
-import {Document} from './document';
+import { Component } from '@angular/core';
+import { Document } from './document';
 
 
 @Component({
 	moduleId: module.id,
 	selector: 'documents',
 	templateUrl: 'documents.component.html'
+	styleUrls:['documents.component.css']
 })
 
 export class DocumentsComponent{
 	pageTitle: string = "Documents Dashboard"
+
 	documents: Document[] = [{
 		title:"My first doc",
 		description:"this is the my first Angular project",
 		file_url: "http://google.com",
 		updated_at:"12/01/17",
-		image_url:" http://google.com",
+		image_url:"https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg",
 		},
 		{
 		title:"My Second doc",
 		description:" you already know , im going to knock it out of the park ",
 		file_url: "http://google.com",
-		updated_at:"12/01/17",
-		image_url:" http://google.com",
+		updated_at:"12/05/17",
+		image_url:"https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg",
 		},
 		{
 		title:"My Third doc",
 		description:"yurpp  thats right Angular project on a role, yup yup ",
 		file_url: "http://google.com",
-		updated_at:"12/01/17",
-		image_url:" http://google.com",
+		updated_at:"12/09/17",
+		image_url:"https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg",
 		}
 	]
 }
